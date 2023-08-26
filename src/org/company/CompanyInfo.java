9ea44afd274792b3e7b09eb1f72b3901c52da4bb
sangithaNehru <sangitha127@gmail.com>
2023-08-26 19:58:06 +0530

    my project

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,20 @@
+package org.company;
+
+public class CompanyInfo { 
+private void companyName() {
+System.out.println("Company Name i TCS");
+} 
+private void CompanyId() {
+System.out.println("Company Id is 12345");
+} 
+private void companyAddress() { 
+System.out.println("Company Address is omr chennai");
+} 
+public static void main(String[] args) {
+CompanyInfo c=new CompanyInfo(); 
+c.companyName(); 
+c.CompanyId(); 
+c.companyAddress();
+}
+
+}
